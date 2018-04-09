f646094f5b208d30f040a10475039cb9768d195e
LeonardoSá <32676285+leodesa@users.noreply.github.com>
2018-04-09 19:48:18 -0300

    Tudo

---
+++ MetaBirl.ino
@@ -0,0 +1,163 @@
+#include <HC05.h>
+
+#define R1F 2
+#define R1T 3
+#define R2F 4
+#define R2T 5
+#define R3F 6
+#define R3T 7
+#define R4F 8
+#define R4T 9
+#define LED 10
+
+// Posição das Rodas no carro:
+//  #####
+//  1###2
+//  #####
+//  3###4
+//  #####
+// Rodas: R(1,2,3,4)Frente(F),Traz(T)
+
+
+int comando;
+int led;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(R1F, OUTPUT);
+  pinMode(R1T, OUTPUT);
+  pinMode(R2F, OUTPUT);
+  pinMode(R2T, OUTPUT);
+  pinMode(R3F, OUTPUT);
+  pinMode(R3T, OUTPUT);
+  pinMode(R4F, OUTPUT);
+  pinMode(R4T, OUTPUT);
+  
+  digitalWrite(R1F, LOW);
+  digitalWrite(R1T, LOW);
+  digitalWrite(R2F, LOW);
+  digitalWrite(R2T, LOW);
+  digitalWrite(R3F, LOW);
+  digitalWrite(R3T, LOW);
+  digitalWrite(R4F, LOW);
+  digitalWrite(R4T, LOW);
+
+  Serial.begin(38400);
+}
+void irFrente(){
+  digitalWrite(R1F, HIGH);
+  digitalWrite(R2F, HIGH);
+  digitalWrite(R3F, HIGH);
+  digitalWrite(R4F, HIGH);
+  delay(25);
+  parar();
+}
+void irTras(){
+  digitalWrite(R1T, HIGH);
+  digitalWrite(R2T, HIGH);
+  digitalWrite(R3T, HIGH);
+  digitalWrite(R4T, HIGH);
+  delay(25);
+  parar();
+}
+void irEsquerdaFrente(){ //G - 71
+  digitalWrite(R1T, HIGH);
+  digitalWrite(R2F, HIGH);
+  digitalWrite(R3F, HIGH);
+  digitalWrite(R4F, HIGH);
+  delay(25);
+  parar();
+}
+void irDireitaFrente(){ //I - 73
+  digitalWrite(R2T, HIGH);
+  digitalWrite(R1F, HIGH);
+  digitalWrite(R3F, HIGH);
+  digitalWrite(R4F, HIGH);
+  delay(25);
+  parar();
+}
+void irEsquerdaTras(){ //H - 72
+  digitalWrite(R1F, HIGH);
+  digitalWrite(R2T, HIGH);
+  digitalWrite(R3T, HIGH);
+  digitalWrite(R4T, HIGH);
+  delay(25);
+  parar();
+}
+void irDireitaTras(){ //J - 74
+  digitalWrite(R2F, HIGH);
+  digitalWrite(R1T, HIGH);
+  digitalWrite(R3T, HIGH);
+  digitalWrite(R4T, HIGH);
+  delay(25);
+  parar();
+}
+void irEsquerda(){
+  digitalWrite(R1T, HIGH);
+  digitalWrite(R2F, HIGH);
+  digitalWrite(R3T, HIGH);
+  digitalWrite(R4F, HIGH);
+  delay(25);
+  parar();
+}
+void irDireita(){
+  digitalWrite(R1F, HIGH);
+  digitalWrite(R2T, HIGH);
+  digitalWrite(R3F, HIGH);
+  digitalWrite(R4T, HIGH);
+  delay(25);
+  parar();
+}
+void parar(){
+  digitalWrite(R1F, LOW);
+  digitalWrite(R1T, LOW);
+  digitalWrite(R2F, LOW);
+  digitalWrite(R2T, LOW);
+  digitalWrite(R3F, LOW);
+  digitalWrite(R3T, LOW);
+  digitalWrite(R4F, LOW);
+  digitalWrite(R4T, LOW);
+}
+void acender(){
+  digitalWrite(LED, HIGH);
+}
+void apagar(){
+  digitalWrite(LED, LOW);
+}
+
+void loop() {
+  if(Serial.available() >0 ){
+    comando = Serial.read();
+    if(comando == 70){
+      irFrente();
+    }else
+    if(comando == 82){
+      irDireita();
+    }else
+    if(comando == 76){
+      irEsquerda();
+    }else
+    if(comando == 66){
+      irTras();
+    }else 
+    if(comando == 73){
+      irDireitaFrente();
+    }else
+    if(comando == 71){
+      irEsquerdaFrente();
+    }else
+    if(comando == 74){
+      irDireitaTras();
+    }else
+    if(comando == 72){
+      irDireitaFrente();
+    }else
+    if(comando == 87){
+      acender();
+    }else 
+    if(comando == 119){
+      apagar();
+    }
+   }
+  }
+
